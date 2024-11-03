38416f2ebded4c498d11e3a650e16295a03de303
Unni Krishnan <ambady1960@hotmail.com>
2024-11-03 14:21:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,23 +5,23 @@ from openai import OpenAI
 st.title("📄 Add New BCA Regulatory Requirements.")
 st.write( "Authorised BCA officer can upload new Requlation here– GPT will update its answer!! " )
 
+
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 #openai_api_key = st.text_input("OpenAI API Key", type="password")
+
+# Create an OpenAI client.
 openai_api_key = st.secrets["openai"]["secret_key"]
+client = OpenAI(api_key=openai_api_key)
 
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
-
+    context = "Building and Construction Authority (BCA), Singapore Building Regulations, and BCA Submission Requirements.\n"
     # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
-    )
+    uploaded_file = st.file_uploader( "Upload a document (.txt or .md)", type=("txt", "md") )
 
     # Ask the user for a question via `st.text_area`.
     question = st.text_area(
@@ -37,13 +37,13 @@ else:
         messages = [
             {
                 "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",   #Add Prefix here....UNNI 3 Nov
+                "content": f"Here's a document: {context + document} \n\n---\n\n {question}",   #Add Prefix here....UNNI 3 Nov
             }
         ]
 
         # Generate an answer using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model= "gpt-4o-mini",
             messages=messages,
             stream=True,
         )
